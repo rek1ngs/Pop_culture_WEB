193dc74b2ba25030601d8555da643a33d6227b38
Cody140 <codyleocadi@gmail.com>
2025-05-23 13:45:45 +0300

    sss

--- POP_Star_Micro.js
+++ POP_Star_Micro.js
@@ -1,1 +1,14 @@
 
+  function incrementAttempts() {
+    let attempts = parseInt(localStorage.getItem('quizAttempts') || '0');
+    attempts += 1;
+    localStorage.setItem('quizAttempts', attempts);
+  }
+
+  document.addEventListener('DOMContentLoaded', () => {
+  const count = localStorage.getItem('quizAttempts');
+  const counter = document.getElementById('attempts-counter');
+  if (count && counter) {
+    counter.textContent = `Šis ir tavs ${count}. mēģinājums`;
+  }
+});
